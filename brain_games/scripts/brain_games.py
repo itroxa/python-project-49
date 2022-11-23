60e6da658fbd70af5506a8aac5de8c170a0ce5f8
Vyacheslav Terskikh <troxa843@gmail.com>
2022-11-23 21:49:16 +0300

    Minor fixes

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.cli import welcome_user
+import brain_games.cli
 
 
 def main():
     print("Welcome to the Brain Games!")
-    welcome_user()
+    brain_games.cli.welcome_user()
 
 
 if __name__ == '__main__':
