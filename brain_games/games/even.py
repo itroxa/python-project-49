a9d631b3d8009b5d72207e8c7de3bca7edc84218
Vyacheslav Terskikh <troxa843@gmail.com>
2022-12-06 13:37:23 +0300

    Ref: is_even() is predicate function

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,15 +5,15 @@ import random
 GAME_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def parity_check(number):
-    if number % 2 == 0:
-        return "yes"
-    else:
-        return "no"
+def is_even(number):
+    return number % 2 == 0
 
 
 def generate_qa():
     number = random.randint(1, 1000)
     question = f"Question: {number}"
-    correct_answer = parity_check(number)
+    if is_even(number):
+        correct_answer = "yes"
+    else:
+        correct_answer = "no"
     return question, correct_answer
