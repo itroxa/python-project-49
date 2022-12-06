8317d67e126b5e62a68d986b97f55b1a473fac8b
Vyacheslav Terskikh <troxa843@gmail.com>
2022-12-06 13:35:54 +0300

    Ref: find_gcd() instead of gcd_check()

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,18 +5,18 @@ import random
 GAME_RULES = 'Find the greatest common divisor of given numbers.'
 
 
-def gcd_check(first_value, second_value):
+def find_gcd(first_value, second_value):
     while first_value != second_value:
         if first_value > second_value:
             first_value = first_value - second_value
         else:
             second_value = second_value - first_value
-    return str(first_value)
+    return first_value
 
 
 def generate_qa():
     first_value = random.randint(1, 100)
     second_value = random.randint(1, 100)
     question = f"Question: {first_value} {second_value}"
-    correct_answer = gcd_check(first_value, second_value)
+    correct_answer = str(find_gcd(first_value, second_value))
     return question, correct_answer
